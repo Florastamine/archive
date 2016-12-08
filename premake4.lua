8970a2e529b30c86c98bfd2d7c219df506cc5416
Florastamine <remilla.scarlet.94@gmail.com>
2016-12-08 10:43:10 +0700

    Updated Premake

--- premake4.lua
+++ premake4.lua
@@ -18,11 +18,13 @@ files {
 
 configuration "Debug" 
 buildoptions {
+    "-Og", -- https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html 
     "-Wall", 
     "-std=gnu++11", 
     "-I\"./Source/3rd/\"",
     "-I\"./Source/Kernel/\"",
-    "-I\"./Source/Kernel/IO/\"",
+    "-I\"./Source/Kernel/IO/\"", 
+    "-I\"./Source/Kernel/Platform/\"",
     "-I\"./Source/Kernel/Compatibility\""
 }
 
@@ -34,5 +36,6 @@ buildoptions {
     "-I\"./Source/3rd/\"", 
     "-I\"./Source/Kernel/\"",   
     "-I\"./Source/Kernel/IO/\"", 
+    "-I\"./Source/Kernel/Platform/\"",  
     "-I\"./Source/Kernel/Compatibility\""
 }
