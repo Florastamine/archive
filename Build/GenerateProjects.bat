c47be41b7327a0a849b5ccf0c9efe966daa93dc3
Florastamine <remilla.scarlet.94@gmail.com>
2016-12-20 16:21:31 +0700

    Separate targets for VS2012, 2013 and 2015

--- Build/GenerateProjects.bat
+++ Build/GenerateProjects.bat
@@ -5,9 +5,21 @@ call ICommon.bat
 
 rem Batch is a retarded language  
 for /f "delims=" %%f in (.\BuildTargetList) do (
-    if %%f == "gmake" (
+    IF %%f == "gmake" (
         %INVOKE_PREMAKE% --cc=gcc --os=linux --platform=x32 %%f
     ) ELSE (
-        %INVOKE_PREMAKE% %%f
+	%INVOKE_PREMAKE% %%f 
+
+	IF "%%f" == "vs2012" ( 
+		rename Hotland.sln Hotland_VS2012.sln 
+	)
+
+	IF "%%f" == "vs2013" (
+		rename Hotland.sln Hotland_VS2013.sln
+	)
+
+	IF "%%f" == "vs2015" (
+		rename Hotland.sln Hotland_VS2015.sln
+	)
     )
 ) 
